03848aee49d6c1d48082d7c3fc1bac1822754088
ALPOMEK <univ.fikrialpome@gmail.com>
2026-07-13 14:14:08 +0700

    Update cleaning_tematik.py

--- cleaning_tematik.py
+++ cleaning_tematik.py
@@ -98,6 +98,6 @@ for idx, row in df_bersih.iterrows():
 # 4. Simpan ke CSV Baru
 df_final = pd.DataFrame(new_rows)
 os.makedirs(r'data\processed', exist_ok=True)
-df_final.to_csv(r'data\processed\master_tematik_2025_cleaned.xlsx', index=False)
+df_final.to_csv(r'data\processed\master_tematik_2025_cleaned.csv', index=False)
 
 print("✅ ETL Tematik 2025 Sukses! Format siap digunakan untuk Dashboard Streamlit.")
